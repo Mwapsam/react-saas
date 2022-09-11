f0d8eccbe9b8d8e628457b816549fdf57f408111
Mwapsam <mwapsam@gmail.com>
2022-09-11 12:05:39 +0200

    Create chart component

--- src/componenets/index.js
+++ src/componenets/index.js
@@ -2,7 +2,9 @@ import Sidebar from './Sidebar';
 import Button from './Button';
 import Table from './Table';
 import Header from './Header';
+import Cards from './Cards';
+import Charts from './Charts';
 
 export {
-  Sidebar, Button, Table, Header,
+  Sidebar, Button, Table, Header, Cards, Charts,
 };
