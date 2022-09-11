b104f295ad52998eee53a200d5fa060f605bbb46
Mwapsam <mwapsam@gmail.com>
2022-09-11 11:02:03 +0200

    Create the music component

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Dashboard from './Dashboard';
+import Vendors from './Vendors';
+import Music from './Music';
+import AddMusic from './AddMusic';
+import Analytics from './Analytics';
+import Settings from './Settings';
+
+export {
+  Dashboard, Vendors, Settings, Analytics, AddMusic, Music,
+};
