1eaa9093e5fd1ca1c6d263f532ba444e48f3624c
Mwapsam <mwapsam@gmail.com>
2022-09-11 17:31:16 +0200

    Style side bar

--- src/componenets/Sidebar.jsx
+++ src/componenets/Sidebar.jsx
@@ -67,9 +67,9 @@ const Sidebar = () => (
             </Link>
           </li>
           <li className="relative px-6 py-3">
-            <a
+            <Link
               className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-              to="/"
+              to="/analytics"
             >
               <svg
                 className="w-5 h-5"
@@ -85,8 +85,8 @@ const Sidebar = () => (
                   d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
                 />
               </svg>
-              <span className="ml-4">Cards</span>
-            </a>
+              <span className="ml-4">Analytics</span>
+            </Link>
           </li>
           <li className="relative px-6 py-3">
             <a
@@ -132,74 +132,6 @@ const Sidebar = () => (
               </svg>
               <span className="ml-4">Buttons</span>
             </a>
-          </li>
-          <li className="relative px-6 py-3">
-            <a
-              className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-              href="modals.html"
-            >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-                />
-              </svg>
-              <span className="ml-4">Modals</span>
-            </a>
-          </li>
-          <li className="relative px-6 py-3">
-            <a
-              className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-              href="tables.html"
-            >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M4 6h16M4 10h16M4 14h16M4 18h16" />
-              </svg>
-              <span className="ml-4">Tables</span>
-            </a>
-          </li>
-          <li className="relative px-6 py-3">
-            <button
-              className="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                // @click="togglePagesMenu"
-              aria-haspopup="true"
-            >
-              <span className="inline-flex items-center">
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"
-                  />
-                </svg>
-                <span className="ml-4">Pages</span>
-              </span>
-
-            </button>
           </li>
         </ul>
         <div className="px-6 my-6">
