2388ec0d629cd88c98b468989c123d83093bd762
Mwapsam <mwapsam@gmail.com>
2022-09-11 12:05:25 +0200

    Create tables in music page

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Sidebar } from '../componenets';
+import { Header, Sidebar, Table } from '../componenets';
 
 const Music = () => (
-  <div>
+  <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
     <Sidebar />
-    <h1>Music</h1>
+    <div className="flex flex-col flex-1 w-full">
+      <Header />
+      <main className="h-full overflow-y-auto">
+        <div className="container px-6 mx-auto grid">
+          <h2
+            className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
+          >
+            Dashboard
+          </h2>
+          <Table />
+        </div>
+      </main>
+    </div>
   </div>
 );
 
