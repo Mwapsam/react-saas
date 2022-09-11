a42d2b5c8cf69cf5fb2046503c97df8c8265affd
Mwapsam <mwapsam@gmail.com>
2022-09-11 17:33:50 +0200

    Call track api endpoint

---
+++ src/services/index.js
@@ -0,0 +1,6 @@
+import getCategories, { createCategories } from './category.service';
+import getTracks, { createTracks } from './track.service';
+
+export {
+  getCategories, createCategories, getTracks, createTracks,
+};
